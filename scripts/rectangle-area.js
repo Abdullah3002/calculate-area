748e712e8068729df3e3049527176c0992cf9fed
Abdullah3002 <zubaerislam703@gmail.com>
2024-05-01 14:32:21 +0600

    second

--- scripts/rectangle-area.js
+++ scripts/rectangle-area.js
@@ -1,3 +1,18 @@
 function calculateRectangleArea(){
-    console.log('calculate rect area');
+    // Get length of the rectangle
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value; // Corrected 'Value' to 'value'
+    const length = parseFloat(lengthText);
+
+    // Get width of the rectangle
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value; // Corrected 'Value' to 'value'
+    const width = parseFloat(widthText);
+
+    // Calculate rectangle area
+    const area = length * width;
+
+    // Display rectangle area
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
